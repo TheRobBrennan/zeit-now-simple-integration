079b153ce48ccc14c7baa195d595ca91f9bcfc90
Rob Brennan <rob@therobbrennan.com>
2019-06-01 13:40:06 -0700

    Our integration is now using example 02 by default

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const { withUiHook, htm } = require('@zeit/integration-utils')  // https://zeit.co/docs/integrations#htm-support/why-use-htm
-const metadataCounter = require('./examples/01-metadata-counter')
+const { withUiHook } = require('@zeit/integration-utils')  // https://zeit.co/docs/integrations#htm-support/why-use-htm
 
-module.exports = withUiHook(metadataCounter)
+// Choose the example code you would like to run
+// const handler = require('./examples/01-metadata-counter')
+const handler = require('./examples/02-explore-integrations')
+
+module.exports = withUiHook(handler)
