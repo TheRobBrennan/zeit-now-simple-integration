6d0bb04493ff83754838d53a4076ca43d1fee5df
Rob Brennan <rob@therobbrennan.com>
2019-06-01 01:05:53 -0700

    Returning output string using new htm import

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const { withUiHook} = require('@zeit/integration-utils')
+const { withUiHook, htm } = require('@zeit/integration-utils')  // https://zeit.co/docs/integrations#htm-support/why-use-htm
 
 let count = 0
 
 /*
-  Every time the button is clicked, the UIHook is called - updating the counter and providing the updated HTML as a response
+  Every time the button is clicked, the UIHook is called - updating the counter and providing the updated HTML as a response.
+
+  "A UIHook accepts a set of inputs, such as a token or projectId, and sends back a string of HTML, including components, as the response.
+
+  With JavaScript template literals, this is relatively straightforward. However, there are a few things to be aware of:
+
+    + Escaping prop names and values
+    + Escaping the content that goes into components
+    + No array.map() support
+    + Difficult to share common components across different Integrations and teams" - https://zeit.co/docs/integrations#htm-support/background
+
+  By using htm, we can effectively write JSX as we want - even using array.map(), passing prop names/values, etc - without transpilation.
+
 */
 module.exports = withUiHook(({ payload }) => {
   count++
-  return `
+  return htm`
     <Page>
       <P>Counter: ${count}</P>
       <Button>Count Me</Button>
