19bcf75aa893e179fc705452b57c35db5a2379ca
Rob Brennan <rob@therobbrennan.com>
2019-06-01 03:45:19 -0700

    Placeholder test updated

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,7 @@
-describe(`Jest should`, () => {
-  it(`be configured`, () => {
+const Integration = require('./index')
 
+describe(`Our ZEIT Now Integration`, () => {
+  it('should be defined', () => {
+    expect(Integration).toBeDefined()
   })
 })
