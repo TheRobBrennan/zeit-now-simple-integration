7297ad820662c51ae2a760d3ce99c9e020969043
Rob Brennan <rob@therobbrennan.com>
2019-06-01 13:56:52 -0700

    Simplified example 02

--- src/examples/02-explore-integrations/index.js
+++ src/examples/02-explore-integrations/index.js
@@ -1,23 +1,12 @@
 const { htm } = require('@zeit/integration-utils')
 
-module.exports = async ({ payload, zeitClient }) => {
-  // const { action } = payload  // See https://zeit.co/docs/integrations#understanding-uihooks/http-payload for more payload details
+const logObject = require('../../lib/logObject')
 
-  // // Initialize metadata store for this configuration ID (max size 100 KB)
-	// const metadata = await zeitClient.getMetadata();
-
-  /*
-
-    Want to create a new secret/environment variable? See https://zeit.co/docs/integrations#project-level-apis/utilities-inside-zeit-integration-utils
-
-  */
-
-  // // Store our count value in the metadata for this specific configuration ID
-  // await zeitClient.setMetadata(metadata)
+module.exports = async (integrationObject) => {
+  logObject(integrationObject, "integrationObject")
 
   return htm`
     <Page>
-      <P>EXPLORE: ZEIT Now Integrations</P>
     </Page>
   `
 }
