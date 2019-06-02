85b8afedc40940560dc84f47657a507c0aae0686
Rob Brennan <rob@therobbrennan.com>
2019-06-02 02:44:34 -0700

    Added supportedActions

--- src/examples/04-google-analytics/lib/constants.js
+++ src/examples/04-google-analytics/lib/constants.js
@@ -1,5 +1,10 @@
 const appIdentifier = '[Example 04: Google Analytics]'
+const supportedActions = {
+  "create-ga-secret": "create-ga-secret",
+  view: "view",
+}
 
 module.exports = {
-  appIdentifier
+  appIdentifier,
+  supportedActions,
 }
