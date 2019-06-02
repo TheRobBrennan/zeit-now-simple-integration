eed4285ae4e88dfd9142cee57a9c9da0fd8c25fd
Rob Brennan <rob@therobbrennan.com>
2019-06-01 17:19:05 -0700

    Completed crude implementation of example 03

---
+++ src/examples/03-zeit-now-deployments/index.js
@@ -0,0 +1,36 @@
+const { htm } = require('@zeit/integration-utils')
+
+// API
+const zeit = require('../../api/zeit/zeit-now-api')
+
+// Useful functions and libraries
+const logObject = require('../../lib/logObject')
+const logValue = require('../../lib/logValue')
+
+// Custom components
+
+module.exports = async ({ payload, zeitClient }) => {
+  let timingResultInMs
+  const NUMBER_OF_DEPLOYMENTS = 100
+  const startOfRequest = Date.now()
+  const { action, clientState } = payload
+
+  // What are we working with?
+  logValue(action, 'action: ')
+  logObject(clientState, 'clientState')
+
+  // Grab our deployments
+  console.log(`Requesting data for up to ${NUMBER_OF_DEPLOYMENTS} deployment(s)...`)
+  const deployments = await zeit.getDeployments(zeitClient, NUMBER_OF_DEPLOYMENTS)
+  timingResultInMs = Date.now() - startOfRequest
+  console.log(`...received data for ${deployments.length} deployment(s)`)
+
+  return htm`
+    <Page>
+      <H1>ZEIT Now Deployments</H1>
+      <B>Loaded ${deployments.length} deployment(s) - ${timingResultInMs / 1000}s</B>
+      <P></P>
+      <Code>${JSON.stringify(deployments, null, 2)}</Code>
+    </Page>
+  `
+}
