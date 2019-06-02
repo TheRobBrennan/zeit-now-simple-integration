25217f191b577b2d4cc956cdc5c444e9ab68d65f
Rob Brennan <rob@therobbrennan.com>
2019-06-01 21:13:48 -0700

    Incorporated new logging module

--- src/examples/04-google-analytics/index.js
+++ src/examples/04-google-analytics/index.js
@@ -5,11 +5,7 @@ const zeit = require('../../api/zeit/zeit-now-api')
 
 // Useful functions and libraries
 const { appIdentifier } = require('./lib/constants')
-// TODO: Consolidate logging
-const logError = require('../../lib/logError')
-const logMessage = require('../../lib/logMessage')
-const logObject = require('../../lib/logObject')
-const logValue = require('../../lib/logValue')
+const log = require('../../lib/log/log')
 // TODO: Consolidate configuration
 const readConfiguration = require('./lib/configuration/readConfiguration')
 const saveConfiguration = require('./lib/configuration/saveConfiguration')
@@ -19,24 +15,24 @@ module.exports = async ({ payload, zeitClient }) => {
   const { action } = payload
 
   // DEBUG
-  logValue(action, `${appIdentifier} Page received action: `)
+  log.value(action, `${appIdentifier} Page received action: `)
 
   // Initialize metadata store for this specific integration configuration (max size 100 KB)
   try {
     metadata = await readConfiguration(zeitClient)
   } catch (e) {
-    logError(e)
+    log.error(e)
   }
 
   // Store our metadata for this specific integration configuration
   try {
     await saveConfiguration(zeitClient, metadata)
   } catch (e) {
-    logError(e)
+    log.error(e)
   }
 
   // DEBUG
-  logMessage(`${appIdentifier} Page render complete`)
+  log.message(`${appIdentifier} Page render complete`)
 
   // TODO: Find a way to programmatically display content 😁
   return htm`
