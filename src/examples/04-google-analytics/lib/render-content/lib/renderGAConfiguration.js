53b7695d4a7d35d263a373f0da00aab0555e1e9e
Rob Brennan <rob@therobbrennan.com>
2019-06-02 15:57:34 -0700

    Updated TODOs and used new ProjectSelector

--- src/examples/04-google-analytics/lib/render-content/lib/renderGAConfiguration.js
+++ src/examples/04-google-analytics/lib/render-content/lib/renderGAConfiguration.js
@@ -2,8 +2,11 @@ const { htm } = require('@zeit/integration-utils')
 const { appIdentifier, supportedActions } = require('../../constants')
 const log = require('../../../../../lib/log/log')
 
+const ProjectSelector = require('../../../components/ProjectSelector')
+// TODO: Create a project configuration component
+// TODO: Do not display configuration form if a project has not been selected
+
 module.exports = ({ clientState, payload }) => {
-  // TODO: Get project and projectId values
   const { project, projectId } = payload
   const { "ga-tracking-id": trackingID, "zeit-now-secret": secretForZEITNow } = clientState
   const googleAnalyticsTrackingID = trackingID || ''
@@ -25,13 +28,9 @@ module.exports = ({ clientState, payload }) => {
     `,
   })
 
-  // TODO: Create a project selector component
-  // TODO: Create a project configuration component with an isVisible prop
-  // TODO: Do not display configuration form if a project has not been selected
   // Return our rendered content
   return htm`
-  <H2>Please select a project</H2>
-  <ProjectSwitcher />
+  <${ProjectSelector} />
   <Box display="flex" paddingTop="8px" paddingBottom="8px" />
   <H2>Configure Google Analytics</H2>
   <Box display="flex" paddingTop="2px">
