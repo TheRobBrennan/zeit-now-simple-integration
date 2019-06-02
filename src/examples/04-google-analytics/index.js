b49377be310fde07b4c2a3d5dfb5bc447c6d1703
Rob Brennan <rob@therobbrennan.com>
2019-06-01 20:13:58 -0700

    Using new readConfiguration lib in UIHook

--- src/examples/04-google-analytics/index.js
+++ src/examples/04-google-analytics/index.js
@@ -4,28 +4,47 @@ const { htm } = require('@zeit/integration-utils')
 const zeit = require('../../api/zeit/zeit-now-api')
 
 // Useful functions and libraries
+const { appIdentifier } = require('./lib/constants')
+const logError = require('../../lib/logError')
+const logMessage = require('../../lib/logMessage')
 const logObject = require('../../lib/logObject')
 const logValue = require('../../lib/logValue')
+const readConfiguration = require('./lib/configuration/readConfiguration')
 
 module.exports = async ({ payload, zeitClient }) => {
-  let timingResultInMs
-  const appIdentifier = '[Example 04: Google Analytics]'
-  const startOfRequest = Date.now()
+  let metadata
   const { action } = payload
 
-  logValue(action, `${appIdentifier} Received action: `)
-  switch (action) {
-    case 'view':  // Initial load
-      console.log(`${appIdentifier} Initial load`)
-      break
-    default:
-      break
+  // DEBUG
+  logValue(action, `${appIdentifier} Page received action: `)
+
+  // Initialize metadata store for this specific integration configuration (max size 100 KB)
+  try {
+    metadata = await readConfiguration(zeitClient)
+  } catch (e) {
+    logError(e)
   }
-  timingResultInMs = Date.now() - startOfRequest
+
+  // switch (action) {
+  //   case 'view':  // Initial load
+  //     console.log(`${appIdentifier} Initial load`)
+  //     break
+  //   default:
+  //     break
+  // }
+
+  // Store our metadata for this specific integration configuration
+  // await zeitClient.setMetadata(metadata)
+
+  // TODO: Render initial view
+  // TODO: Render welcome screen
+  // TODO: Render page timing result
+
+  // DEBUG
+  logMessage(`${appIdentifier} Page render complete`)
 
   return htm`
     <Page>
-      <B>Page loaded in ${timingResultInMs}ms</B>
     </Page>
   `
 }
