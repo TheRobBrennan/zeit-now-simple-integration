0bdf1ad8b4107cbc6a459bf4d54fca6314d3ac89
Rob Brennan <rob@therobbrennan.com>
2019-06-02 14:05:07 -0700

    Using clientState to set input values

--- src/examples/04-google-analytics/lib/render-content/lib/renderGAConfiguration.js
+++ src/examples/04-google-analytics/lib/render-content/lib/renderGAConfiguration.js
@@ -3,19 +3,34 @@ const { appIdentifier, supportedActions } = require('../../constants')
 const log = require('../../../../../lib/log/log')
 
 module.exports = ({ clientState }) => {
-  log.message({ message: `${appIdentifier} rendering Google Analytics configuration` })
-  log.entity({ obj: clientState, label: `${appIdentifier} renderGAConfiguration received clientState ` })
-  // TODO: Use clientState to populate values of form field
+  const { "ga-tracking-id": trackingID, "zeit-now-secret": secretForZEITNow } = clientState
+  const googleAnalyticsTrackingID = trackingID || ''
+  const zeitNowSecretForGoogleAnalyticsTrackingID = secretForZEITNow || ''
+
+  // Logging
+  log.message({
+    message: `${appIdentifier} rendering Google Analytics configuration`,
+  })
+  log.entity({
+    obj: clientState,
+    label: `${appIdentifier} renderGAConfiguration received clientState `,
+  })
+
   // TODO: Add project selector
   // TODO: Disable button until a valid tracking ID and secret have been entered
+  // TODO: Disable button if the fields match a known secret we've created using this integration
   // TODO: Add error states/messaging
   return htm`
     <H2>Configure Google Analytics</H2>
     <Box display="flex" width="80%">
-      <Input name="ga-tracking-id" label="GA Tracking ID" placeholder="UA-########-##" maxLength="15" width="80%" value="UA-68343119-5" />
-      <Input name="zeit-now-secret" label="ZEIT Now secret" placeholder="GOOGLE_ANALYTICS_TRACKING_ID" width="150%" value="GOOGLE_ANALYTICS_TRACKING_ID" />
+      <Input name="ga-tracking-id" label="GA Tracking ID" placeholder="UA-########-##" maxLength="15" width="90%"
+        value="${googleAnalyticsTrackingID}" />
+      <Input name="zeit-now-secret" label="ZEIT Now secret" placeholder="GOOGLE_ANALYTICS_TRACKING_ID" width="150%"
+        value="${zeitNowSecretForGoogleAnalyticsTrackingID}" />
       <Box paddingTop="22px" paddingLeft="20%">
-        <Button action="${supportedActions["create-ga-secret"]}">Create Secret</Button>
+        <Button action="${
+          supportedActions['create-ga-secret']
+        }">Create Secret</Button>
       </Box>
     </Box>
   `
