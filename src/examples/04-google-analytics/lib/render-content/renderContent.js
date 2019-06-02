d7c96d6216e4603bb8868f924627843a3dc7fe95
Rob Brennan <rob@therobbrennan.com>
2019-06-01 22:16:57 -0700

    Displaying Welcome component on initial page load

--- src/examples/04-google-analytics/lib/render-content/renderContent.js
+++ src/examples/04-google-analytics/lib/render-content/renderContent.js
@@ -2,8 +2,23 @@ const { appIdentifier } = require('../constants')
 const { htm } = require('@zeit/integration-utils')
 const log = require('../../../../lib/log/log')
 
+// Components
+const Welcome = require('../../components/Welcome')
+
 module.exports = (action) => {
-  log.value(action, `${appIdentifier} renderContent received action: `)
+  let renderedOutput
+
+  log.message(`${appIdentifier} renderContent received action "${action}"`)
+
+  switch (action) {
+    case 'view':  // Initial load
+      renderedOutput = htm`<${Welcome} message=${"Welcome"} tag=${"H1"} />`
+      break
+    default:
+      break
+  }
+
   log.message(`${appIdentifier} renderContent complete`)
-  return htm``
+
+  return htm`${renderedOutput}`
 }
