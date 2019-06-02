cae3e89936464cc0a6ce79b6b65a2740c94ab44a
Rob Brennan <rob@therobbrennan.com>
2019-06-01 17:43:51 -0700

    Added placeholder functionality for example 04

---
+++ src/examples/04-google-analytics/index.js
@@ -0,0 +1,32 @@
+const { htm } = require('@zeit/integration-utils')
+
+// API
+const zeit = require('../../api/zeit/zeit-now-api')
+
+// Useful functions and libraries
+const logObject = require('../../lib/logObject')
+const logValue = require('../../lib/logValue')
+
+module.exports = async ({ payload, zeitClient }) => {
+  let timingResultInMs
+  const appIdentifier = '[Example 04: Google Analytics]'
+  const startOfRequest = Date.now()
+  const { action } = payload
+
+  logValue(action, `${appIdentifier} Received action: `)
+  switch (action) {
+    case 'view':  // Initial load
+      console.log(`${appIdentifier} Initial load`)
+      break
+    default:
+      break
+  }
+  timingResultInMs = Date.now() - startOfRequest
+
+  return htm`
+    <Page>
+      <H1>[DEMO] Google Analytics</H1>
+      <B>${timingResultInMs}ms</B>
+    </Page>
+  `
+}
