d2b03e0e8314e737be310f6555530c551cd7d602
Rob Brennan <rob@therobbrennan.com>
2019-06-01 22:20:10 -0700

    Minor cleanup of example 04

--- src/examples/04-google-analytics/index.js
+++ src/examples/04-google-analytics/index.js
@@ -1,15 +1,14 @@
 const { htm } = require('@zeit/integration-utils')
 
 // Useful functions and libraries
-const { appIdentifier } = require('./lib/constants')
-const log = require('../../lib/log/log')
 const configuration = require('./lib/configuration/configuration')
-const { readConfiguration, saveConfiguration } = configuration
+const log = require('../../lib/log/log')
 const renderContent = require('./lib/render-content/renderContent')
 
 module.exports = async ({ payload, zeitClient }) => {
   let metadata
   const { action } = payload
+  const { readConfiguration, saveConfiguration } = configuration
 
   // Initialize metadata store for this specific integration configuration (max size 100 KB)
   try {
